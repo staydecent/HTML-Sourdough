c5168f08647792df70923b29ff7e34023ce7ad65
Adrian Unger <dev@staydecent.ca>
2013-04-08 23:59:02 -0700

    Adds ability to copy static files (txt|ico) from root to public.

--- build.sh
+++ build.sh
@@ -7,9 +7,17 @@ set -e
 # 
 
 [ -d ./public ] && rm -R ./public
-mkdir ./public
+mkdir -p ./public/js
 
 cp -r app/static public/static
+cp app/index.html public/index.html
+
+cd ./app
+ls | egrep 'txt|ico' | while read -r file
+do
+    cp "${file}" "../public/${file}"
+done
+cd ../
 
 
 # ----------
@@ -25,7 +33,7 @@ libsBefore=(
 libsJoined+=$(printf ",%s" "${libsBefore[@]}")
 libsJoined=${libsJoined:1}
 
-components=`python ./scripts/concat.py 'components' 'public/components.js' ${libsJoined} | tail -n 1`
+components=`python ./scripts/concat.py 'components' 'public/js/components.js' ${libsJoined} | tail -n 1`
 if [ -z "$components" ]; then
   echo "Error concatenating components"
   exit 1
@@ -37,7 +45,7 @@ fi
 # ----------
 # App
 
-app=`python ./scripts/concat.py 'app' 'public/app.js' | tail -n 1`
+app=`python ./scripts/concat.py 'app' 'public/js/app.js' | tail -n 1`
 if [ -z "$app" ]; then
   echo "Error concatenating app"
   exit 1
