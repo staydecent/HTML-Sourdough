a0c03f3aec8a103f024f473cf9b460826f90b875
Adrian Unger <dev@staydecent.ca>
2013-06-09 11:00:32 -0700

    Removes default styles and updates build to use adhesive.

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash 
 set -e
 
-# 
-# Build
-#  Read scripts/concat.py to figure this out
-# 
-
 [ -d ./public ] && rm -R ./public
 mkdir -p ./public/js
+
+# 
+# Copy static files
+# 
 
 cp -r app/static public/static
 cp app/index.html public/index.html
@@ -19,36 +18,8 @@ do
 done
 cd ../
 
+# 
+# Run build_js
+# 
 
-# ----------
-# Components
-
-declare -a libsBefore
-libsBefore=(
-  'jquery.min.js' 
-  'underscore-min.js' 
-  'backbone-min.js'
-)
-
-libsJoined+=$(printf ",%s" "${libsBefore[@]}")
-libsJoined=${libsJoined:1}
-
-components=`python ./scripts/concat.py 'components' 'public/js/components.js' ${libsJoined} | tail -n 1`
-if [ -z "$components" ]; then
-  echo "Error concatenating components"
-  exit 1
-else
-  echo $components
-fi
-
-
-# ----------
-# App
-
-app=`python ./scripts/concat.py 'app' 'public/js/app.js' | tail -n 1`
-if [ -z "$app" ]; then
-  echo "Error concatenating app"
-  exit 1
-else
-  echo $app
-fi
+tools/build_js/node_modules/adhesive/adhesive.js tools/build_js/app.build.json --debug
